415c08d919b6cf4a7566c5b4d203b1a9907c1043
Sander van Harmelen <sander@vanharmelen.nl>
2019-11-05 14:12:24 +0100

    initial version

---
+++ main.tf
@@ -0,0 +1,156 @@
+locals {
+  connect_vcs_repo = var.connect_vcs_repo ? { create = true } : {}
+}
+provider "aws" {}
+
+module "workspace_account" {
+  providers   = { aws = aws }
+  source      = "github.com/schubergphilis/terraform-aws-mcaf-user?ref=v0.1.3"
+  name        = var.username
+  policy      = var.policy
+  policy_arns = var.policy_arns
+  tags        = var.tags
+}
+
+resource "github_repository" "default" {
+  count              = var.create_repository ? 1 : 0
+  name               = var.github_repository
+  description        = var.repository_description
+  allow_rebase_merge = false
+  allow_squash_merge = false
+  auto_init          = true
+  has_downloads      = false
+  has_issues         = false
+  has_projects       = false
+  has_wiki           = false
+  private            = true
+}
+
+resource "github_team_repository" "admins" {
+  count      = length(var.github_admins)
+  team_id    = var.github_admins[count.index]
+  repository = var.github_repository
+  permission = "admin"
+
+  depends_on = [github_repository.default]
+}
+
+resource "github_team_repository" "writers" {
+  count      = length(var.github_writers)
+  team_id    = var.github_writers[count.index]
+  repository = var.github_repository
+  permission = "push"
+
+  depends_on = [github_repository.default]
+}
+
+resource "tfe_workspace" "default" {
+  name                  = var.name
+  organization          = var.terraform_organization
+  auto_apply            = var.auto_apply
+  file_triggers_enabled = var.file_triggers_enabled
+  terraform_version     = var.terraform_version
+  trigger_prefixes      = var.trigger_prefixes
+  queue_all_runs        = true
+  working_directory     = var.working_directory
+
+  dynamic vcs_repo {
+    for_each = local.connect_vcs_repo
+
+    content {
+      identifier         = "${var.github_organization}/${var.github_repository}"
+      branch             = var.branch
+      ingress_submodules = false
+      oauth_token_id     = var.oauth_token_id
+    }
+  }
+
+  depends_on = [github_repository.default]
+}
+
+resource "tfe_notification_configuration" "default" {
+  count                 = var.slack_notification_url != null ? 1 : 0
+  name                  = tfe_workspace.default.name
+  enabled               = true
+  destination_type      = "slack"
+  url                   = var.slack_notification_url
+  workspace_external_id = tfe_workspace.default.external_id
+
+  triggers = [
+    "run:created",
+    "run:planning",
+    "run:needs_attention",
+    "run:applying",
+    "run:completed",
+    "run:errored"
+  ]
+}
+
+resource "tfe_variable" "aws_account_arn" {
+  key          = "aws_account_arn"
+  value        = module.workspace_account.arn
+  category     = "terraform"
+  workspace_id = tfe_workspace.default.id
+}
+
+resource "tfe_variable" "aws_access_key_id" {
+  key          = "AWS_ACCESS_KEY_ID"
+  value        = module.workspace_account.access_key_id
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.default.id
+}
+
+resource "tfe_variable" "aws_secret_access_key" {
+  key          = "AWS_SECRET_ACCESS_KEY"
+  value        = module.workspace_account.secret_access_key
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.default.id
+}
+
+resource "tfe_variable" "aws_default_region" {
+  key          = "AWS_DEFAULT_REGION"
+  value        = var.region
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.default.id
+}
+
+resource "tfe_variable" "clear_text_env_variables" {
+  for_each = var.clear_text_env_variables
+
+  key          = each.key
+  value        = each.value
+  category     = "env"
+  workspace_id = tfe_workspace.default.id
+}
+
+resource "tfe_variable" "sensitive_env_variables" {
+  for_each = var.sensitive_env_variables
+
+  key          = each.key
+  value        = each.value
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.default.id
+}
+
+resource "tfe_variable" "clear_text_terraform_variables" {
+  for_each = var.clear_text_terraform_variables
+
+  key          = each.key
+  value        = each.value
+  category     = "terraform"
+  workspace_id = tfe_workspace.default.id
+}
+
+resource "tfe_variable" "sensitive_terraform_variables" {
+  for_each = var.sensitive_terraform_variables
+
+  key          = each.key
+  value        = each.value
+  category     = "terraform"
+  sensitive    = true
+  workspace_id = tfe_workspace.default.id
+}
---
+++ variables.tf
@@ -0,0 +1,155 @@
+variable "name" {
+  type        = string
+  description = "A name for the Terraform workspace"
+}
+
+variable "region" {
+  type        = string
+  default     = null
+  description = "The default region of the account"
+}
+
+variable "auto_apply" {
+  type        = bool
+  default     = false
+  description = "Whether to automatically apply changes when a Terraform plan is successful"
+}
+
+variable "branch" {
+  type        = string
+  default     = "master"
+  description = "The Git branch to trigger the TFE workspace for"
+}
+
+variable "clear_text_env_variables" {
+  type        = map(string)
+  default     = {}
+  description = "An optional map with clear text environment variables"
+}
+
+variable "clear_text_terraform_variables" {
+  type        = map(string)
+  default     = {}
+  description = "An optional map with clear text Terraform variables"
+}
+
+variable "create_repository" {
+  type        = bool
+  default     = false
+  description = "Whether of not to create a new repository"
+}
+
+variable "connect_vcs_repo" {
+  type        = bool
+  default     = true
+  description = "Whether or not to connect a VCS repo to the workspace"
+}
+
+variable "file_triggers_enabled" {
+  type        = bool
+  default     = true
+  description = "Whether to filter runs based on the changed files in a VCS push"
+}
+
+variable "github_admins" {
+  type        = list(string)
+  default     = []
+  description = "A list of Github teams that should have admins access"
+}
+
+variable "github_organization" {
+  type        = string
+  description = "The Github organization to connect the workspace to"
+}
+
+variable "github_repository" {
+  type        = string
+  description = "The Github organization to connect the workspace to"
+}
+
+variable "github_writers" {
+  type        = list(string)
+  default     = []
+  description = "A list of Github teams that should have write access"
+}
+
+variable "kms_key_id" {
+  type        = string
+  default     = null
+  description = "The KMS key ID used to encrypt the SSM parameters"
+}
+
+variable "oauth_token_id" {
+  type        = string
+  description = "The OAuth token ID of the VCS provider"
+}
+
+variable "policy" {
+  type        = string
+  default     = null
+  description = "The policy to attach to the pipeline user"
+}
+
+variable "policy_arns" {
+  type        = set(string)
+  default     = []
+  description = "A set of policy ARNs to attach to the pipeline user"
+}
+
+variable "repository_description" {
+  type        = string
+  default     = null
+  description = "A description for the Github repository"
+}
+
+variable "sensitive_env_variables" {
+  type        = map(string)
+  default     = {}
+  description = "An optional map with sensitive environment variables"
+}
+
+variable "sensitive_terraform_variables" {
+  type        = map(string)
+  default     = {}
+  description = "An optional map with sensitive Terraform variables"
+}
+
+variable "slack_notification_url" {
+  type        = string
+  default     = null
+  description = "The Slack Webhook URL to send notification to"
+}
+
+variable "terraform_version" {
+  type        = string
+  default     = "latest"
+  description = "The version of Terraform to use for this workspace"
+}
+
+variable "terraform_organization" {
+  type        = string
+  description = "The Terraform Enterprise organization to create the workspace in"
+}
+
+variable "trigger_prefixes" {
+  type        = list(string)
+  default     = ["modules"]
+  description = "List of repository-root-relative paths which should be tracked for changes"
+}
+
+variable "username" {
+  type        = string
+  default     = null
+  description = "The username for a new pipeline user."
+}
+
+variable "working_directory" {
+  type        = string
+  default     = "terraform"
+  description = "A relative path that Terraform will execute within"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A mapping of tags to assign to resource"
+}
