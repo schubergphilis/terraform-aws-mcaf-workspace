b23443d58b1f661aa9f3e4683bc77493cc4df3fe
Stephen Hoekstra <shoekstra@schubergphilis.com>
2021-05-20 17:15:03 +0200

    Remove VCS providers
    We tried to support multiple VCS providers and due to how some written this does not give the best user experience. We tried to several work arounds but ultimately VCS repos should be managed outside of this module and passed into this module creating an implicit dependency so they're created before the workspace is created (and then attached to).
    
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
-output "repo_full_name" {
-  value       = var.create_repository ? module.github_repository[0].full_name : null
-  description = "The full 'organization/repository' name of the repository"
-}
-
 output "workspace_id" {
   value       = tfe_workspace.default.id
-  description = "The Terraform workspace ID"
+  description = "The Terraform Cloud workspace ID"
 }
