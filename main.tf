4108789f617847cd0d623c6c7d56c778eb0624e5
Shreya Vashisht <svashisht@schubergphilis.com>
2023-01-09 16:08:26 +0100

    adding arn of boundary

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "workspace_iam_role" {
   name                 = var.role_name
   role_policy          = var.policy
   policy_arns          = var.policy_arns
-  permissions_boundary = resource.aws_iam_policy.ep_infra_boundary
+  permissions_boundary = aws_iam_policy.ep_infra_boundary.arn
   tags                 = var.tags
 
   assume_policy = templatefile("${path.module}/templates/assume_role_policy.tftpl", {
