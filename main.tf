ebc9ff20a8136a8c55ee0501c10893a5f8c9b632
Sjoerd Tromp <stromp@schubergphilis.com>
2023-02-01 17:33:42 +0100

    Fix terraform fmt

--- main.tf
+++ main.tf
@@ -37,8 +37,8 @@ module "workspace_iam_role" {
   tags                 = var.tags
 
   assume_policy = templatefile("${path.module}/templates/assume_role_policy.tftpl", {
-    external_id = random_uuid.external_id[0].result,
-    role_arns_json    = jsonencode(var.agent_role_arns)
+    external_id    = random_uuid.external_id[0].result,
+    role_arns_json = jsonencode(var.agent_role_arns)
   })
 }
 
--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "agent_pool_id" {
 }
 
 variable "agent_role_arns" {
-  type        = list
+  type        = list(any)
   default     = null
   description = "IAM role ARN used by Terraform Cloud Agent to assume role in the created account"
 }
