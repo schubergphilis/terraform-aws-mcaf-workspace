83fb168b1943783a667bc3e55acc10ba118c7a27
Stephen Hoekstra <shoekstra@schubergphilis.com>
2024-03-04 15:45:00 +0100

    fix(output): Add missing `workload_name` output
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "arn" {
   description = "The workspace IAM user ARN"
 }
 
+output "workspace_name" {
+  value       = tfe_workspace.default.name
+  description = "The Terraform Cloud workspace name"
+}
+
 output "workspace_id" {
   value       = tfe_workspace.default.id
   description = "The Terraform Cloud workspace ID"
