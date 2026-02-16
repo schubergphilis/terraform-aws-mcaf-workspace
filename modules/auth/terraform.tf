5a29b80492b65db4cd6db93fe019fd025b3f15d1
Marwin Baumann <mbaumann@schubergphilis.com>
2026-02-16 13:58:18 +0100

    add terraform.tf to module

---
+++ modules/auth/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = ">= 0.67.1"
+    }
+  }
+}
